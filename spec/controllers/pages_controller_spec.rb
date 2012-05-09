cf5abb30fdfaa9b3f18fce0faf997bc69f26d4bb
gabriel-dehan <dehan.gabriel@gmail.com>
2012-05-09 18:31:40 +0200

    Action name is now displayed in title

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,12 +8,22 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+
+    it "should have a correct title" do
+      get 'home'
+      response.should have_selector("title", content: "| Home")
+    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+
+    it "should have a correct title" do
+      get 'contact'
+      response.should have_selector("title", content: "| Contact")
     end
   end
 
@@ -22,6 +32,10 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
-  end
 
+    it "should have a correct title" do
+      get 'about'
+      response.should have_selector("title", content: "| About")
+    end
+  end
 end
